be39560cca0b9c7fcbb97afcbb33f96f18dc2983
tylerwong <tjwong@dons.usfca.edu>
2018-10-27 00:06:07 -0700

    finished constructor and add function

--- ArrayList.java
+++ ArrayList.java
@@ -24,4 +24,50 @@ public class ArrayList implements List {
         arr[curr++] = obj;
     }
 
+    @Override
+    public void add(int pos, Object obj) {
+        if(curr >= size){
+            Object[] temp = new Object[size*2];
+            for(int i = 0; i < curr; i++){
+                temp[i] = arr[i];
+            }
+            arr = temp;
+            size *= 2;
+        }
+
+        for(int i = curr; i > pos; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[pos] = obj;
+        curr++;
+    }
+
+    @Override
+    public Object get(int pos) {
+        if(pos < 0 || pos >= curr){
+            return 0;
+        }
+        return arr[pos];
+    }
+
+    @Override
+    public Object remove(int pos) {
+        if(pos < 0 || pos >= curr){
+            System.out.println("The index does not exist in the ArrayList.");
+            return 0;
+        }
+
+        for(int i = pos + 1; i < curr; i++){
+            arr[i-1] = arr[i];
+        }
+        curr--;
+        return arr[pos];
+    }
+
+    @Override
+    public int size() {
+        return curr;
+    }
+
+
 }
